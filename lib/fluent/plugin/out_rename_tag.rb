a79d1ee0d6aae075ec43c99477c66f2ada92ade6
sangwook <2sangwook@gmail.com>
2015-02-27 15:46:21 +0900

    key 를 앞에 붙이지 않도록 수정.

--- lib/fluent/plugin/out_rename_tag.rb
+++ lib/fluent/plugin/out_rename_tag.rb
@@ -5,7 +5,7 @@ module Fluent
     def emit(tag, es, chain)
       es.each { |time, record|
         split_tag = tag.split('.')
-        new_tag = "#{split_tag.last[0,5]}_#{split_tag.first}_#{split_tag[1..-2].join('_')}".gsub(/[^0-9a-zA-Z_]/, '_')
+        new_tag = "#{split_tag.first}_#{split_tag[1..-2].join('_')}".gsub(/[^0-9a-zA-Z_]/, '_')
         new_tag = "runway.#{new_tag}"
         Engine.emit(new_tag, time, record)
       }
